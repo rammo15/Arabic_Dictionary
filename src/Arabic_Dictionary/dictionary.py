b922a3eeef984a3cffdd025d489fac5d94567041
Rami Sulaimani <46478430+rammo15@users.noreply.github.com>
2026-08-11 15:38:17 +0300

    Update dictionary.py

--- src/Arabic_Dictionary/dictionary.py
+++ src/Arabic_Dictionary/dictionary.py
@@ -1,1 +1,27 @@
+from .models import Word
 
+
+class Dictionary:
+
+    def __init__(self):
+        self._providers = []
+
+    def register_provider(self, provider):
+
+        self._providers.append(provider)
+
+    def lookup(self, text: str):
+
+        for provider in self._providers:
+
+            result = provider.lookup(text)
+
+            if result:
+
+                return result
+
+        return None
+
+    def exists(self, text):
+
+        return self.lookup(text) is not None
