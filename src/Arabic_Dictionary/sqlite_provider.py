d2a06a4af763963894345d6bbc437b92da7f2bd1
Rami Sulaimani <46478430+rammo15@users.noreply.github.com>
2026-08-11 16:18:13 +0300

    Update sqlite_provider.py

--- src/Arabic_Dictionary/sqlite_provider.py
+++ src/Arabic_Dictionary/sqlite_provider.py
@@ -1,1 +1,15 @@
+from .providers import Provider
+from .storage import SQLiteStorage
 
+
+class SQLiteProvider(Provider):
+
+    name = "SQLite"
+
+    def __init__(self, storage: SQLiteStorage):
+
+        self.storage = storage
+
+    def lookup(self, word):
+
+        return self.storage.get(word)
