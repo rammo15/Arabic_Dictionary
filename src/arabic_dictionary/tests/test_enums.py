84e85276fb4635289625411aa872b0f859a8858e
Rami Sulaimani <46478430+rammo15@users.noreply.github.com>
2026-08-12 14:11:10 +0300

    Update test_enums.py

--- src/arabic_dictionary/tests/test_enums.py
+++ src/arabic_dictionary/tests/test_enums.py
@@ -1,1 +1,12 @@
+from arabic_dictionary.domain.enums import RelationType
+from arabic_dictionary.domain.enums import WordType
 
+
+def test_word_type_enum():
+
+    assert WordType.NOUN.value == "noun"
+
+
+def test_relation_enum():
+
+    assert RelationType.SYNONYM.value == "synonym"
