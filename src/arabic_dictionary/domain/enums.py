49ab85dc8b0b1a0e03a98b45ebddc6de82c9ce98
Rami Sulaimani <46478430+rammo15@users.noreply.github.com>
2026-08-12 10:55:31 +0300

    Update enums.py

--- src/arabic_dictionary/domain/enums.py
+++ src/arabic_dictionary/domain/enums.py
@@ -1,1 +1,26 @@
+from enum import Enum
 
+
+class WordType(str, Enum):
+    """أنواع الكلمات."""
+
+    NOUN = "noun"
+    VERB = "verb"
+    ADJECTIVE = "adjective"
+    ADVERB = "adverb"
+    PARTICLE = "particle"
+    PHRASE = "phrase"
+    UNKNOWN = "unknown"
+
+
+class RelationType(str, Enum):
+    """العلاقات بين الكلمات."""
+
+    SYNONYM = "synonym"
+    ANTONYM = "antonym"
+    DERIVED = "derived"
+    RELATED = "related"
+    PLURAL = "plural"
+    SINGULAR = "singular"
+    FEMININE = "feminine"
+    MASCULINE = "masculine"
